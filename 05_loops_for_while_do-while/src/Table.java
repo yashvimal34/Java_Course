4d43a2b2766316365e44d172e32e25a9494d6d40
Yash Vimal <yashsarthak79@gmail.com>
2026-03-13 22:06:53 +0530

    .

--- 05_loops_for_while_do-while/src/Table.java
+++ 05_loops_for_while_do-while/src/Table.java
@@ -1,6 +1,6 @@
 public class Table {
-    public static void main(String[] args){
-        for(int i = 5; i<=50; i+=5){
+    public static void main(String[] args) {
+        for (int i = 5; i <= 50; i += 5) {
             System.out.println(i);
         }
     }
